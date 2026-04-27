c21385935a883e5a0af2ad270bd78fc135c1de70
inf505 <inf505@protonmail.com>
2026-04-27 07:30:36 -0600

    ...

--- life-plus/app.js
+++ life-plus/app.js
@@ -2,25 +2,13 @@ const { createApp, ref, onMounted, nextTick } = Vue;
 
 const app = createApp({
   setup() {
-    // Game State
-    const isPlaying = ref(false);
-
-    // UI State (Added this)
     const isSidebarOpen = ref(false);
 
     const toggleSidebar = () => {
       isSidebarOpen.value = !isSidebarOpen.value;
     };
 
-    const togglePlay = () => {
-      isPlaying.value = !isPlaying.value;
-      // Re-render Lucide icons after Vue updates the DOM
-      nextTick(() => lucide.createIcons());
-    };
-
-    onMounted(() => {
-      lucide.createIcons(); // Initial icon load
-    });
+    onMounted(() => {});
 
     // Everything returned here is available to your HTML
     return {
